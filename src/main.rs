17e9026efa6965bfe5fb9c85b925018a9f3c64e2
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2026-08-04 19:48:38 +0000

    basic ui is defined

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use macroquad::prelude::*;
-use pathfinder::cell::{Cell, CellState};
+use pathfinder::{
+    AppState,
+    cell::{Cell, CellState},
+};
 
 const GRID_W: usize = 40;
 
@@ -16,8 +19,14 @@ async fn main() {
     }
 
     // println!("{:#?}", cells);
+    //
+    let mut app_state = AppState::new();
+
+    let mut help_text = false;
 
     loop {
+        app_state.detect_key();
+
         clear_background(BLACK);
 
         let cell_size = (screen_width() / GRID_W as f32).min(screen_height() / GRID_H as f32);
@@ -25,19 +34,12 @@ async fn main() {
         // draw grid
         //
         for cell in &cells {
-            let color = match cell.state() {
-                CellState::Empty => LIGHTGRAY,
-                CellState::Start => GREEN,
-                CellState::Goal => YELLOW,
-                CellState::Wall => BROWN,
-                CellState::Path => BLUE,
-            };
             draw_rectangle(
                 cell.width() as f32 * cell_size,
                 cell.height() as f32 * cell_size,
                 cell_size - 1.0,
                 cell_size - 1.0,
-                color,
+                cell.state().color(),
             );
         }
 
@@ -57,12 +59,67 @@ async fn main() {
         if gx < GRID_W && gy < GRID_H {
             let index = gx * GRID_H + gy;
 
-            if is_mouse_button_down(MouseButton::Left) {
-                cells[index].set_state(CellState::Wall);
+            if let Some(select_state) = app_state.get_state()
+                && is_mouse_button_down(MouseButton::Left)
+            {
+                match select_state {
+                    CellState::Empty => cells[index].set_state(CellState::Empty),
+                    CellState::Start => cells[index].set_state(CellState::Start),
+                    CellState::Goal => cells[index].set_state(CellState::Goal),
+                    CellState::Wall => cells[index].set_state(CellState::Wall),
+                    _ => {}
+                }
             }
+
+            if cells[index].state() == &CellState::Empty {}
         }
 
         draw_text("PathFinder", 20.0, 20.0, 30.0, WHITE);
+
+        if is_key_released(KeyCode::H) {
+            help_text = !help_text;
+        }
+        if help_text {
+            draw_text(
+                "Help",
+                screen_width() / 2.0 - 38.0 / 2.0,
+                screen_height() / 2.0,
+                38.0,
+                DARKBLUE,
+            );
+
+            draw_text(
+                "H -- Help",
+                screen_width() / 2.0 - 38.0 / 2.0,
+                screen_height() / 2.0 + 30.0,
+                28.0,
+                DARKBLUE,
+            );
+
+            draw_text(
+                "G -- Set Goal Area",
+                screen_width() / 2.0 - 38.0 / 2.0,
+                screen_height() / 2.0 + 60.0,
+                28.0,
+                DARKBLUE,
+            );
+
+            draw_text(
+                "S -- Set Start Area",
+                screen_width() / 2.0 - 38.0 / 2.0,
+                screen_height() / 2.0 + 90.0,
+                28.0,
+                DARKBLUE,
+            );
+
+            draw_text(
+                "W -- Build Wall",
+                screen_width() / 2.0 - 38.0 / 2.0,
+                screen_height() / 2.0 + 120.0,
+                28.0,
+                DARKBLUE,
+            );
+        }
 
         next_frame().await;
     }
