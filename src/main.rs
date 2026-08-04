6f7c38160606475873d5b33dcb4538695b15ef7b
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2026-08-04 06:32:57 +0000

    Gotten mouse position and created cell struct

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 use macroquad::prelude::*;
+use pathfinder::cell::{Cell, CellState};
 
 const GRID_W: usize = 40;
 
 const GRID_H: usize = 30;
-
-#[derive(Debug, Clone)]
-struct Cell {
-    x: usize,
-    y: usize,
-    state: CellState,
-}
-
-#[derive(Debug, Clone)]
-enum CellState {
-    Empty,
-    Start,
-    Goal,
-    Wall,
-    Path,
-}
 
 #[macroquad::main("Pathfinder")]
 async fn main() {
@@ -26,11 +11,7 @@ async fn main() {
 
     for x in 0..GRID_W {
         for y in 0..GRID_H {
-            cells.push(Cell {
-                x,
-                y,
-                state: CellState::Empty,
-            });
+            cells.push(Cell::new(x, y));
         }
     }
 
@@ -44,7 +25,7 @@ async fn main() {
         // draw grid
         //
         for cell in &cells {
-            let color = match cell.state {
+            let color = match cell.state() {
                 CellState::Empty => LIGHTGRAY,
                 CellState::Start => GREEN,
                 CellState::Goal => YELLOW,
@@ -52,12 +33,33 @@ async fn main() {
                 CellState::Path => BLUE,
             };
             draw_rectangle(
-                cell.x as f32 * cell_size,
-                cell.y as f32 * cell_size,
+                cell.width() as f32 * cell_size,
+                cell.height() as f32 * cell_size,
                 cell_size - 1.0,
                 cell_size - 1.0,
                 color,
             );
+        }
+
+        // get position
+
+        let (mx, my) = mouse_position();
+
+        // println!("Mouse {mx}, {my}");
+
+        let (gx, gy) = ((mx / cell_size) as usize, (my / cell_size) as usize);
+
+        // println!("Mouse {gx}, {gy}");
+        //
+
+        // Change state at mouse pos
+        //
+        if gx < GRID_W && gy < GRID_H {
+            let index = gx * GRID_H + gy;
+
+            if is_mouse_button_down(MouseButton::Left) {
+                cells[index].set_state(CellState::Wall);
+            }
         }
 
         draw_text("PathFinder", 20.0, 20.0, 30.0, WHITE);
